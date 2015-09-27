7a39b89c99796fc157ae87c108c19629646cd22a
julian <jcvgalfonso@gmail.com>
2015-09-27 15:36:25 -0500

    Test for simple logout implemented
    Test for simple logout implemented

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,7 @@
 # Create your tests here.
 from django.test import TestCase
 from dspace import DSpace
+import httplib
 __author__ = 'JULIAN'
 
 
@@ -12,9 +13,9 @@ class TestDSpace(TestCase):
       """
       dspace = DSpace("http://45.55.192.223:8443/rest")
       #user created for testing purposes#
-      expected_response_code = 200
+      expected_response_code = httplib.OK #200
       response = dspace.login("api_user@example.com","passw0rd_api")
-      self.assertEqual(expected_response_code,response.status_code, "Login success test fails")
+      self.assertTrue(dspace.api_key != "","Login success test fails, API Key not assigned")
 
   def test_login_fail(self):
       """
@@ -22,7 +23,20 @@ class TestDSpace(TestCase):
       """
       dspace = DSpace("http://45.55.192.223:8443/rest")
       #user created for testing purposes#
-      expected_response_code = 403
+      expected_response_code = httplib.FORBIDDEN #403
       response = dspace.login("api_user@example.com","not_my_password")
       self.assertEqual(expected_response_code,response.status_code, "Login error test fails")
+
+  def test_logout(self):
+      """
+      Tests Logout
+      :return:
+      """
+      dspace = DSpace("http://45.55.192.223:8443/rest")
+      success_response_code = httplib.OK #200
+      bad_request_code = httplib.BAD_REQUEST #403
+      #login before test log out
+      dspace.login("api_user@example.com","passw0rd_api")
+      response = dspace.logout()
+      self.assertEqual(response.status_code,success_response_code,"Logout test fails")
 
