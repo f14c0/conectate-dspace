20e5764089bdb6b70f6b0107438bdf3a2bb32a0f
julian <jcvgalfonso@gmail.com>
2015-09-28 23:02:16 -0500

    New tests added
    Following tests implemented
    -Test for get_all_items limiting results
    -Test for get_item including metadata
    
    Minor code clean
    
    TODO Refactor in new Item tests in new TestCase

--- api/tests.py
+++ api/tests.py
@@ -9,9 +9,11 @@ __author__ = 'JULIAN'
 
 
 class TestDSpace(TestCase):
+  #TODO Refactor in new Item tests in new TestCase
 
   def setUp(self):
-      self.rest_api_path = "http://45.55.192.223:8443/rest"
+      #self.rest_api_path = "http://45.55.192.223:8443/rest"
+      self.rest_api_path = "https://trydspace.longsight.com/rest"
       self.dspace = DSpace(self.rest_api_path)
       pass
 
@@ -62,20 +64,38 @@ class TestDSpace(TestCase):
       items = Item.get_all(dspace)
       self.assertGreater(len(items),0)
 
+  def test_get_all_items_with_limit(self):
+      """
+      Test Retrieving all items with limit
+      """
+      dspace= self.dspace
+      max_items = 5
+      items = Item.get_all(dspace,limit=max_items)
+      self.assertLessEqual(len(items),max_items)
+
   def test_get_item(self):
       """
       Test Retrieving an item by Id
       """
       dspace= self.dspace
-      test_id = 5
+      test_id = 55
       item = Item.get_item(dspace,test_id)
       self.assertEqual(item.id,test_id,"Get item by Id Fails")
+
+  def test_get_item_with_metadata(self):
+      """
+      Test Retrieving an item by Id
+      """
+      dspace= self.dspace
+      test_id = 55
+      item = Item.get_item(dspace,test_id,expand="metadata")
+      self.assertIsNotNone(item.metadata,"Item metadata not retrieved")
 
   def test_get_item_metadata(self):
       """
       Test Retrieving an item by Id
       """
       dspace= self.dspace
-      test_id = 5
+      test_id = 55
       metadata = Item.get_item_metadata(dspace,test_id)
       self.assertGreater(len(metadata),0)
