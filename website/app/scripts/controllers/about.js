9f62bc73bec75276a31b0067db90757056b98dad
julian <jcvgalfonso@gmail.com>
2015-09-29 21:55:49 -0500

    Project refactor

---
+++ website/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name conectateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the conectateApp
+ */
+angular.module('conectateApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
