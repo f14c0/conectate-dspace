c715e6a15fa70b4c46b20643300d40a695ca7b58
Julian Vega <jc.vega11@uniandes.edu.co>
2015-11-01 20:57:47 -0800

    [CONT-245][CONT-246] Basic rest endpoint for Users
    Basic rest endpoint for users implemented
    TODO: Aditional user atributes

--- api/views.py
+++ api/views.py
@@ -9,6 +9,11 @@ from rest_framework.decorators import api_view
 from dspace import Item,DSpace,Collection,Community,Bitstream
 from Conectate.settings import DSPACE_REST_ENDPOINT
 
+
+from django.contrib.auth.models import User
+from api.serializers import UserSerializer
+
+
 import json
 
 dspace = DSpace(DSPACE_REST_ENDPOINT)
@@ -226,3 +231,52 @@ class BitstreamDetail(APIView):
         pass
 
         bitstream
+
+class UserList(APIView):
+    """
+    List all users, or create a new user.
+    """
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserDetail(APIView):
+    """
+    Retrieve, update or delete a user instance.
+    """
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user = UserSerializer(user)
+        return Response(user.data)
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
